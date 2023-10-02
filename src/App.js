5cf04818a8421775ed8ef8650c0e758805c4bf9e
Rimsha23 <imrimshamalik2023@gmail.com>
2023-10-02 22:42:27 +0500

    packages and libraries installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react'
 
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: '#'
+  },
+]);
 function App() {
   return (
     <div className="App">
-     
+      <RouterProvider router={router} />
     </div>
   );
 }
